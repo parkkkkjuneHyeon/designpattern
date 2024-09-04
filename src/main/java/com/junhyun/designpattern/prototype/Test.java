9279d03481aac09a685b8bf7d77f498ca23a964b
박준현 <gktkwk91@naver.com>
2024-09-04 13:38:47 +0900

    프로토타입 패턴 구현

---
+++ src/main/java/com/junhyun/designpattern/prototype/Test.java
@@ -0,0 +1,29 @@
+package com.junhyun.designpattern.prototype;
+
+import com.junhyun.designpattern.prototype.pattern.Book;
+import com.junhyun.designpattern.prototype.pattern.BookShelf;
+
+public class Test {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        BookShelf bookShelf = new BookShelf();
+
+        bookShelf.add(new Book("이용범", "열한번째 사과나무"));
+        bookShelf.add(new Book("J. K. 롤링", "해리포터 마법사의 돌"));
+        bookShelf.add(new Book("J. K. 롤링", "해리포터 비밀의 방"));
+        bookShelf.add(new Book("J. K. 롤링", "해리포터 아즈카반의 죄수"));
+
+        System.out.println(bookShelf);
+        BookShelf bookShelf2 = bookShelf.getCloneBookShelf();
+        System.out.println("------------------------------------------------");
+        System.out.println(bookShelf2);
+
+        bookShelf.getBook(1).setTitle("해리포터 죽음의 성물 1");
+        System.out.println("------------------------------------------------");
+
+        System.out.println("bookShelf" + bookShelf);
+        System.out.println("------------------------------------------------");
+        System.out.println("bookShelf2" + bookShelf2);
+
+
+    }
+}
---
+++ src/main/java/com/junhyun/designpattern/prototype/pattern/BookShelf.java
@@ -0,0 +1,40 @@
+package com.junhyun.designpattern.prototype.pattern;
+
+import java.util.ArrayList;
+
+public class BookShelf implements Cloneable{
+    private ArrayList<Book> books;
+
+    public BookShelf() {
+        books = new ArrayList<>();
+    }
+    public void add(Book book) {
+        books.add(book);
+    }
+    public Book getBook(int index) {
+        return books.get(index);
+    }
+    public BookShelf getCloneBookShelf() throws CloneNotSupportedException {
+        return (BookShelf) clone();
+    }
+
+    @Override
+    public String toString() {
+        return "BookShelf{" +
+                "books=" + books +
+                '}';
+    }
+
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        BookShelf bookShelf = new BookShelf();
+
+        //얕은 복사를 하지않기 위해 새롭게 생성자를 생성해서 같은 데이터를 넣어줌.
+        //즉 다른 메모리 주소에 Book객체를 생성해서 같은 데이터를 주입
+        for (Book book : books)
+            bookShelf.add(new Book(book.getAuthor(), book.getTitle()));
+
+        return bookShelf;
+    }
+}
